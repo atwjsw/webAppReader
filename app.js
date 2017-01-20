35ae0201acb390a32516fc0a61c65fcdc0c6f12a
Daniel Wen <daniel.wen@ericsson.com>
2017-01-21 01:37:04 +0800

    initial commit chapter 11-8

---
+++ app.js
@@ -0,0 +1,141 @@
+var koa = require('koa');
+var app = koa();
+var qs = require('querystring');
+
+//路由模块配置
+var controller = require('koa-route');
+
+//模板渲染模块配置
+var views = require('co-views');
+var render = views('./view', {
+    map: { html: 'ejs' }
+});
+
+//静态资源文件模块配置
+var koa_static = require('koa-static-server');
+app.use(koa_static({
+	rootDir:'./static/',
+	rootPath: '/static/',
+	maxage : 0
+}));
+
+var service = require('./service/webAppService');
+
+app.use(controller.get('/route_test', function*() {
+    this.set('Cache-Control', 'no-cache')
+    this.body = 'Hello koa!';
+}));
+
+app.use(controller.get("/ejs_test", function*() {
+	//console.log('render: ' + render('ejs_test', {title: 'ejs_test'}));
+	this.set('Cache-Control', 'no-cache');
+	this.body = yield render('ejs_test', {title: 'ejs_test'});
+	// this.body = render('ejs_test', {title: 'ejs_test'});
+}));
+
+app.use(controller.get("/api_test", function*() {
+	this.set('Cache-Control', 'no-cache');
+    this.body = service.get_test_data();
+}));
+
+app.use(controller.get("/api_test_async", function*() {
+	this.set('Cache-Control', 'no-cache');
+    this.body = yield service.get_test_data_async();
+}));
+
+
+
+
+//
+app.use(controller.get("/ejs_test", function*() {
+	//console.log('render: ' + render('ejs_test', {title: 'ejs_test'}));
+	this.set('Cache-Control', 'no-cache');
+	this.body = yield render('ejs_test', {title: 'ejs_test'});
+	// this.body = render('ejs_test', {title: 'ejs_test'});
+}));
+
+//以下为前端页面请求处理
+app.use(controller.get("/", function*() {
+	this.set('Cache-Control', 'no-cache');
+	this.body = yield render('index', {title: '书城首页'});	
+}));
+
+app.use(controller.get("/category", function*() {
+	this.set('Cache-Control', 'no-cache');
+	this.body = yield render('category', {title: '分类页'});	
+}));
+
+app.use(controller.get("/rank", function*() {
+	this.set('Cache-Control', 'no-cache');
+	this.body = yield render('rank', {title: '排行页'});	
+}));
+
+app.use(controller.get("/search", function*() {
+	this.set('Cache-Control', 'no-cache');
+	this.body = yield render('search', {title: '搜索页'});	
+}));
+
+app.use(controller.get("/book", function*() {
+	this.set('Cache-Control', 'no-cache');	
+	var params = qs.parse(this.req._parsedUrl.query);
+	var bookId = params.id;
+	if (!bookId) {
+		bookId = '';
+	}
+	this.body = yield render('book', {bookId: bookId});		
+}));
+
+
+//以下为后端AJAX请求代码
+app.use(controller.get("/ajax/index", function*() {
+	this.set('Cache-Control', 'no-cache');
+    this.body = service.get_index_data();
+}));
+
+app.use(controller.get("/ajax/rank", function*() {
+	this.set('Cache-Control', 'no-cache');
+    this.body = service.get_rank_data();
+}));
+
+app.use(controller.get("/ajax/category", function*() {
+	this.set('Cache-Control', 'no-cache');
+    this.body = service.get_category_data();
+}));
+
+app.use(controller.get("/ajax/bookbacket", function*() {
+	this.set('Cache-Control', 'no-cache');
+    this.body = service.get_bookbacket_data();
+}));
+
+app.use(controller.get("/ajax/channel/female", function*() {
+	this.set('Cache-Control', 'no-cache');
+    this.body = service.get_channel_female_data();
+}));
+
+app.use(controller.get("/ajax/channel/male", function*() {
+	this.set('Cache-Control', 'no-cache');
+    this.body = service.get_channel_male_data();
+}));
+
+app.use(controller.get("/ajax/book/", function*() {
+	this.set('Cache-Control', 'no-cache');	
+	var params = qs.parse(this.req._parsedUrl.query);
+	var id = params.id;
+	if (!id) {
+		id = '';
+	}
+    this.body = service.get_book_data();
+}));
+
+app.use(controller.get("/ajax/search", function*() {
+	this.set('Cache-Control', 'no-cache');	
+	var params = qs.parse(this.req._parsedUrl.query);
+	var start = params.start;
+	var end =params.end;
+	var keyword = params.keyword;
+    this.body = yield service.get_search_data(start, end, keyword);
+    //this.body = service.get_search_data(start, end, keyword);
+}));
+
+app.listen(3000);
+console.log("Server listened on port 3000");
---
+++ service/webAppService.js
@@ -0,0 +1,99 @@
+var fs = require('fs');
+
+//获取mock数据
+exports.get_test_data = function() {
+    var content = fs.readFileSync('./mock/test.json', 'utf-8');
+    console.log('read data: ' + content);
+    return content;
+};
+
+//获取主页数据
+exports.get_index_data = function() {
+    var content = fs.readFileSync('./mock/home.json', 'utf-8');    
+    return content;
+};
+
+//获取排行数据
+exports.get_rank_data = function() {
+    var content = fs.readFileSync('./mock/rank.json', 'utf-8');    
+    return content;
+};
+
+//获取分类数据
+exports.get_category_data = function() {
+    var content = fs.readFileSync('./mock/category.json', 'utf-8');    
+    return content;
+};
+
+//获取书架数据
+exports.get_bookbacket_data = function() {
+    var content = fs.readFileSync('./mock/get_bookbacket_data.json', 'utf-8');    
+    return content;
+};
+
+//获取女生频道数据
+exports.get_channel_female_data = function() {
+    var content = fs.readFileSync('./mock/channel/female.json', 'utf-8');    
+    return content;
+};
+
+//获取男生频道数据
+exports.get_channel_male_data = function() {
+    var content = fs.readFileSync('./mock/channel/male.json', 'utf-8');    
+    return content;
+};
+
+//获取书内容
+exports.get_book_data = function(id) {
+	if(!id) {
+		id = "18218";
+	}
+    var content = fs.readFileSync('./mock/book/'+ id +'.json', 'utf-8');    
+    return content;
+};
+
+//异步方式获取mock数据
+exports.get_test_data_async = function() {
+	var readfile = function(callback) {
+		fs.readFile('./mock/test.json', 'utf8', (err, data) => {
+  			if (err) throw err;
+  			callback(null, data);
+  		});
+  	};
+  	return readfile;
+  };
+
+//获取http接口数据
+exports.get_search_data = function(start, end, keyword) {
+    // body...
+    return function(cb) {
+        var http = require('http');
+        var qs = require('querystring');
+        var data = { s: keyword, start: start, end: end }
+        var content = qs.stringify(data);
+        var options = {
+            host: 'dushu.xiaomi.com',
+            port: 80,
+            path: '/store/v0/lib/query/onebox?' + content,
+            method: 'GET'
+        };
+
+        var req = http.request(options, function(res) {
+        	var content = '';            
+            res.setEncoding('utf8');
+            res.on('data', function(chunk) {                
+                content += chunk;
+            });
+            res.on('end', function() {
+            	// console.log('res body: ' + content);
+            	cb(null, content);
+            });
+        });
+
+        req.on('error', function(e) {
+            console.log("Error in http request: " + e.message);
+        });
+
+        req.end();
+    };
+};
